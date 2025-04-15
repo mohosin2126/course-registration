0956b18819a02a404520f20cdbf2402060fa41fd
Md Mohosin Ali Shah <mohosin2126@gmail.com>
2025-04-15 10:29:44 +0600

    updated design issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
 import './App.css'
 import MainCard from './Components/Maincard/MainCard'
-import Right from './Components/RightCard/Right'
 
 function App() {
-  
 
-  return (
-    <>
-     <div>
-      <h1 className='text-center mt-10 text-4xl font-bold' >Course Registration</h1>
-     </div>
-      <div className='mt-20 p-5'>
- <div >
- <div className=' '>
-<MainCard></MainCard>
- </div>
- </div>
-      </div>
-    </>
-  )
+
+    return (
+        <>
+            <div className="w-2/3 mx-auto">
+                <div >
+                    <h1 className='text-center mt-10 text-4xl font-bold'>Course Registration</h1>
+                </div>
+                <div className='mt-20'>
+                    <div>
+                        <div>
+                            <MainCard></MainCard>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </>
+    )
 }
 
 export default App
